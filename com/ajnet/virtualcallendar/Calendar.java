01b584ce26f7dcadfef559b25aee27071d4cf82b
user <user@DESKTOP-8C0OHM0>
2020-01-23 12:58:38 +0900

    가상의 달력 print 2

--- com/ajnet/virtualcallendar/Calendar.java
+++ com/ajnet/virtualcallendar/Calendar.java
@@ -5,21 +5,30 @@ import java.util.Scanner;
 public class Calendar {
 		
 		public static final int [] maxday  = {31,28,31,30,31,30,31,31,30,31,30,31};
+		public static final int [] maxday_leaf  = {31,29,31,30,31,30,31,31,30,31,30,31};
 		
-		public static int getMonthDays(int month) {
-		    return maxday[month-1];
+		public static boolean isLeafYear(int year) {
+			if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+			    return true;
+			} else {
+				return false;
+			}
+		}
+		public static int getMonthDays(int year, int month) {
+		    if (isLeafYear(year)) {
+			    return maxday_leaf[month-1];
+			} else {
+				return maxday[month-1];
+			}
 		}
 		
-		public void printCalendar(int year, int month, String yn) {
+		public void printCalendar(int year, int month) {
 			System.out.printf("   <<%4d %3d>> \n", year, month);
 			System.out.println(" 일 월 화 수 목 금 토");
 			System.out.println("---------------------");
 			
-			int maxDay = getMonthDays(month);
-			if(yn == "y") {
-				maxDay = 29;
-			}
-	
+			int maxDay = getMonthDays(year, month);
+				
 			for(int i = 1; i <= maxDay; i++) {
 				System.out.printf("%3d", i);
 				if(i % 7 == 0) {
@@ -37,24 +46,19 @@ public class Calendar {
 			Calendar cal = new Calendar();
 			int year = 0;
 			int month = 1;
-			String yn = "n";
+			 
 			while (true) {
-				System.out.println("년도를 입력하세요");
+				
+				System.out.println("년도와 달을 입력하세요");
 				System.out.print(PROMPT);
-				year = sc.nextInt();
-				if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
-					System.out.println(year + "년은 윤년");
-					//윤년
-					yn = "y"; 
-				} else {
-					System.out.println(year + "년은 평년");
-					//평년
-					yn = "n"; 
-				}
+				String s1;
+				String s2;
+				s1 = sc.next();
+				s2 = sc.next();
+				year = Integer.parseInt(s1);
+				month = Integer.parseInt(s2);
+												
 								
-				System.out.println("달을 입력하세요");
-				System.out.print(PROMPT);
-				month = sc.nextInt();
 				if(month == -1) {
 					break;
 				}
@@ -63,7 +67,7 @@ public class Calendar {
 				}
 				else 
 				{
-					cal.printCalendar(year, month, yn);
+					cal.printCalendar(year, month);
 				}
 			}
 			
